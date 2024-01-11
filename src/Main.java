659905cb19b0e440949655c8f127bc2d952c32b4
juujacometti <julyajacometty@gmail.com>
2024-01-11 18:36:11 -0300

    Finalizado

--- src/Main.java
+++ src/Main.java
@@ -14,41 +14,46 @@ public class Main {
 
 // Looping de continuação:
 
+        do {
 
 // Solicitação p usuário (números):
 
-        System.out.println("Olá! Digite os dois números que você deseja utilizar para realizar a operação. \n1° Número: ");
-        x = leia.nextDouble();
-        System.out.println("\n2° Número: ");
-        y = leia.nextDouble();
+            System.out.println("Olá! Digite os dois números que você deseja utilizar para realizar a operação. \n1° Número: ");
+            x = leia.nextDouble();
+            System.out.println("\n2° Número: ");
+            y = leia.nextDouble();
 
 // Solicitação p usuário (operação):
 
-        System.out.println("Qual das operações você deseja realizar? \n1 -> Soma \n2 -> Subtração \n3 -> Multiplicação \n4 -> Divisão");
-        escolha = leia.nextInt();
+            System.out.println("Qual das operações você deseja realizar? \n1 -> Soma \n2 -> Subtração \n3 -> Multiplicação \n4 -> Divisão");
+            escolha = leia.nextInt();
 
 // Execução da escolha:
 
-        switch (escolha) {
-            case 1:
-                System.out.println("A soma entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.somar(x, y) ) );
-            case 2:
-                System.out.println("A subtração entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.subt(x, y) ) );
-            case 3:
-                System.out.println("A multiplicação entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.mult(x, y) ) );
-            case 4:
-                System.out.println("A divisão entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.divi(x, y) ) );
-        }
+            switch (escolha) {
+                case 1:
+                    System.out.println("A soma entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.somar(x, y)));
+                    break;
+                case 2:
+                    System.out.println("A subtração entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.subt(x, y)));
+                    break;
+                case 3:
+                    System.out.println("A multiplicação entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.mult(x, y)));
+                    break;
+                case 4:
+                    System.out.println("A divisão entre os números " + x + " e " + y + " Resulta em: " + (Operacoes.divi(x, y)));
+                    break;
+            }
 
 // Solicitação p usuário (continuar?):
 
-        System.out.println("Você deseja realizar outra operação? \n 1 -> Sim \n2 -> Não");
-        continuar = leia.nextInt();
-
-
-
+            System.out.println("\nVocê deseja realizar outra operação? \n1 -> Sim \n2 -> Não");
+            continuar = leia.nextInt();
 
         }
 
+        while (continuar == 1);
+
+        System.out.println("Programa encerrado!");
     }
 }
